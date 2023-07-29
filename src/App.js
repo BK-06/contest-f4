f986d66241418052924fcb0713ebbe5882c007d3
Neon <105565447+BK-06@users.noreply.github.com>
2023-07-30 00:09:39 +0530

    path change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import { useEffect, useRef } from "react";
 import Login from "./Login";
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Profile from "./Profile";
 
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="contest-f4" Component={Login} />
+        <Route exact path="" Component={Login} />
+        <Route exact path="profile" Component={Profile} />
+        {/* <Route exact path="data" Component={Screen2} /> */}
+      </Routes>
+    </Router>
 
-export default function App(){
-    
-    return ( 
-
-        <Router>
-            <Routes>
-                <Route exact path="" Component={Login} />
-                <Route exact path="profile" Component={Profile} />
-                {/* <Route exact path="data" Component={Screen2} /> */}
-            </Routes>
-        </Router>
-    
-        // <div>
-        //     <Login/>
-        // </div>
-    )
+    // <div>
+    //     <Login/>
+    // </div>
+  );
 }
